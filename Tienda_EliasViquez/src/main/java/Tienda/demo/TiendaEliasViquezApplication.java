13097976ddbf3c90ac23233efd142f9c4bd4874a
eliasvise <jviquez20768@ufide.ac.cr>
2024-09-25 20:05:54 -0600

    Project creation
    New Project creation

---
+++ Tienda_EliasViquez/src/main/java/Tienda/demo/TiendaEliasViquezApplication.java
@@ -0,0 +1,13 @@
+package Tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaEliasViquezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaEliasViquezApplication.class, args);
+	}
+
+}
